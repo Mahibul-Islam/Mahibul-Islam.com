88e7d58a6a99163d54faaf873c026181bbb8639d
mahibulll <akonratul2gmail.com>
2022-06-13 16:24:07 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// typing script js
+
+var typed = new Typed(".typing", {
+    strings: ["Web Developer", "Web Designer", "problem solver", "Programmer expart", "Web Developer", "Web Designer", "problem solver", "Programmer expart", "Web Developer", "Web Designer", "problem solver", "Programmer expart"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
+
+
+var typed = new Typed(".typing2", {
+    strings: ["Web Developer", "Web Designer", "problem solver", "Programmer expart", "Web Developer", "Web Designer", "problem solver", "Programmer expart", "Web Developer", "Web Designer", "problem solver", "Programmer expart"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
+// Show/Hide FAQs answer
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if (icon.className === 'fa-solid fa-plus') {
+            icon.className = 'fa-solid fa-minus';
+        } else {
+            icon.className = 'fa-solid fa-plus';
+        }
+
+    })
+})
+
+// nav menu
+
+
+//Show/hide nav menu
+const menu = document.querySelector('.nav_menu');
+const menuBtn = document.querySelector('.open_menu_btn');
+const closeBtn = document.querySelector('.colse_menu_btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+closeBtn.addEventListener('click', closeNav);
+
+
+//nav color change on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 100);
+})
